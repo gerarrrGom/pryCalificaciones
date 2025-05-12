55ad3a14d3f83b1dae3710a6393d6478cd492914
JMingo2021 <jose_domingo@live.com.mx>
2025-05-12 11:47:08 -0600

    Clean Architecture
    Clean Architecture

--- app/src/main/java/mx/edu/unpa/calificaciones/providers/MateriaProvider.kt
+++ app/src/main/java/mx/edu/unpa/calificaciones/providers/MateriaProvider.kt
@@ -1,4 +1,28 @@
 package mx.edu.unpa.calificaciones.providers
 
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.firestore
+import mx.edu.unpa.calificaciones.models.Materia
+
 class MateriaProvider {
+    var db = Firebase.firestore.collection("Materia")
+    // var _id: String? = null
+/*
+    fun create(materia : Materia): Task<Void>{
+        materia.materiaId = materia.materiaId ?: db.document().id
+        return db.document(materia.materiaId!!).set(materia)
+    }
+*/
+    fun getGrade(id: String): Query{
+        return db.whereEqualTo("gradeId", id)
+    }
+
+    /*fun getId(): String {
+        return _id.toString()
+    }
+    fun setId(id: String) {
+        _id = id
+    }*/
 }
